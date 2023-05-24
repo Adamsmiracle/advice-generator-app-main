8e96bb9d34af03760cd06d53f8dd5aba7414c407
adams miracle <adamsmiracle0@gmail.com>
2023-05-24 18:11:30 +0000

    finished project

---
+++ app.js
@@ -0,0 +1,48 @@
+const adviceId = document.getElementById("advice-id")
+const adviceText = document.getElementById("advice-text")
+const changeBtn = document.getElementById("change-btn")
+
+
+let numberOfRandomAdvice = 100
+
+
+getAdvice()
+async function getAdvice() {
+    const addvice = await fetch(`https://api.adviceslip.com/advice/${getRandomId()}`)
+    const res = await addvice.json()
+    updateUi(res)
+}
+
+const interval = setInterval(() => {
+    getAdvice()
+}, 30000)
+
+
+function updateUi(response) {
+    const advice = response.slip.advice
+    id = response.slip.id
+
+    adviceText.textContent = ''
+    adviceId.textContent = ''
+
+    adviceText.textContent = advice
+    adviceId.textContent = id
+
+}
+
+
+changeBtn.addEventListener("click", () => {
+    clearInterval(interval)
+    getAdvice()
+    setInterval(() => {
+        getAdvice()
+    }, 30000)
+
+})
+
+
+function getRandomId() {
+    randomId = Math.floor(Math.random() * numberOfRandomAdvice)
+    return randomId
+}
+
